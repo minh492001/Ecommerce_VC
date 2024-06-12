bef1bc24cb7999aedfee527f478dc2ac53903142
MinMin <gow.bin.gg@gmail.com>
2024-06-12 19:55:37 +0700

    feat: create [Add Admin] feature

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+    public function list()
+    {
+        $data['header_title'] = 'Admin';
+        return view('admin.admin.list', $data);
+    }
+
+    public function add()
+    {
+        $data['header_title'] = 'New Admin';
+        return view('admin.admin.add', $data);
+    }
+
+    public function insert(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->status = $request->status;
+        $user->is_admin = 1;
+        $user->save();
+        return redirect('admin/admin/list')->with('success', 'Admin added successfully !');
+    }
+}
---
+++ resources/views/admin/admin/add.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layouts.app')
+@section('style')
+@endsection
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-12">
+                        <h1>Add New Admin</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-primary">
+                            <form action="" method="post">
+                                {{ csrf_field() }}
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Status</label>
+                                        <select class="form-control" name="status">
+                                            <option value="0">Active</option>
+                                            <option value="1">Inactive</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+
+@endsection
+
+@section('script')
+
+@endsection
+
