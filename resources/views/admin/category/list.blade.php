3c13982f699f31c4b3465aa2c48f7af7ee8f78c5
MinMin <gow.bin.gg@gmail.com>
2024-06-17 17:59:02 +0700

    refactor: add Paginate to Category list

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -61,6 +61,9 @@
                                         @endforeach
                                     </tbody>
                                 </table>
+                                <div style="padding: 10px; float: right;">
+                                    {!! $getRecord->appends(\Illuminate\Support\Facades\Request::except('page'))->links() !!}
+                                </div>
                             </div>
                         </div>
                     </div>
