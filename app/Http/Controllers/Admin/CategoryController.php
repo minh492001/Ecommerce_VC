52e791c832e579b98dcf4350648c689c91cbf439
MinMin <gow.bin.gg@gmail.com>
2024-06-17 14:40:59 +0700

    feat: create [Add Category] feature

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+
+class CategoryController extends Controller
+{
+    public function add()
+    {
+        $data['header_title'] = 'Add Category';
+        return view('admin.category.add', $data);
+    }
+
+    public function insert(Request $request)
+    {
+        request()->validate([
+            'slug' => 'required|unique:category',
+        ]);
+
+        $category = new Category;
+        $category->name = trim($request->name);
+        $category->slug = trim($request->slug);
+        $category->status = trim($request->status);
+        $category->meta_title = trim($request->meta_title);
+        $category->meta_description = trim($request->meta_description);
+        $category->meta_keywords = trim($request->meta_keywords);
+        $category->created_by = Auth::user()->id;
+        $category->save();
+
+        return redirect('admin/category/list')->with('success', 'Category added successfully !');
+    }
+}
