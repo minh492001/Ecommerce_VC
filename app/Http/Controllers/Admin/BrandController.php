ab9db0995f6dc2397d69882dd907e4971e5b722f
MinMin <gow.bin.gg@gmail.com>
2024-06-17 22:58:34 +0700

    feat: create [Add Brand] feature

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -16,4 +16,28 @@ class BrandController extends Controller
         return view('admin.brand.list', $data);
     }
 
+    public function add()
+    {
+        $data['header_title'] = 'Add Brand';
+        return view('admin.brand.add', $data);
+    }
+
+    public function insert(Request $request)
+    {
+        request()->validate([
+            'slug' => 'required|unique:brand',
+        ]);
+
+        $brand = new Brand;
+        $brand->name = trim($request->name);
+        $brand->slug = trim($request->slug);
+        $brand->status = trim($request->status);
+        $brand->meta_title = trim($request->meta_title);
+        $brand->meta_description = trim($request->meta_description);
+        $brand->meta_keywords = trim($request->meta_keywords);
+        $brand->created_by = Auth::user()->id;
+        $brand->save();
+
+        return redirect('admin/brand/list')->with('success', 'Brand added successfully !');
+    }
 }
