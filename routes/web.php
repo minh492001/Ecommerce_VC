2f43ed2f60977e05ae74edc8aae499100a32a276
MinMin <gow.bin.gg@gmail.com>
2024-06-24 22:33:52 +0700

    feat: create [Update Product] route

--- routes/web.php
+++ routes/web.php
@@ -80,4 +80,6 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('admin/product/add', [ProductController::class, 'insert']);
 
     Route::get('admin/product/edit/{id}', [ProductController::class, 'edit']);
+    Route::post('admin/product/edit/{id}', [ProductController::class, 'update']);
+
 });
