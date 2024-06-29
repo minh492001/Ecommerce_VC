054bf58504c8661ba2892a350191ac3b126236e7
MinMin <gow.bin.gg@gmail.com>
2024-06-29 12:59:37 +0700

    feat: create [HomePage] view

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,229 @@
+<header class="header">
+    <div class="header-top">
+        <div class="container">
+            <div class="header-left">
+                <div class="header-dropdown">
+                    <a href="#">Usd</a>
+                    <div class="header-menu">
+                        <ul>
+                            <li><a href="#">Eur</a></li>
+                            <li><a href="#">Usd</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="header-dropdown">
+                    <a href="#">Eng</a>
+                    <div class="header-menu">
+                        <ul>
+                            <li><a href="#">English</a></li>
+                            <li><a href="#">French</a></li>
+                            <li><a href="#">Spanish</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="header-right">
+                <ul class="top-menu">
+                    <li>
+                        <a href="#">Links</a>
+                        <ul>
+                            <li><a href="tel:#"><i class="icon-phone"></i>Call: +0123 456 789</a></li>
+                            <li><a href="{{ url('wishlist') }}"><i class="icon-heart-o"></i>My Wishlist <span>(3)</span></a></li>
+                            <li><a href="{{ url('about') }}">About Us</a></li>
+                            <li><a href="{{ url('contact') }}">Contact Us</a></li>
+                            <li><a href="#signin-modal" data-toggle="modal"><i class="icon-user"></i>Login</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-middle sticky-header">
+        <div class="container">
+            <div class="header-left">
+                <button class="mobile-menu-toggler">
+                    <span class="sr-only">Toggle mobile menu</span>
+                    <i class="icon-bars"></i>
+                </button>
+
+                <a href="{{ url('') }}" class="logo">
+                    <img src="{{ url('assets/images/logo.png') }}" alt="" width="105" height="25">
+                </a>
+
+                <nav class="main-nav">
+                    <ul class="menu sf-arrows">
+                        <li class=active">
+                            <a href="{{ url('') }}">Home</a>
+
+                        </li>
+                        <li>
+                            <a href="javascript:" class="sf-with-ul">Shop</a>
+
+                            <div class="megamenu megamenu-md">
+                                <div class="row no-gutters">
+                                    <div class="col-md-12">
+                                        <div class="menu-col">
+                                            <div class="row">
+                                                @php
+                                                    $getCategoryHeader = App\Models\Category::getRecordMenu();
+                                                @endphp
+                                                @foreach($getCategoryHeader as $value_category)
+                                                <div class="col-md-4" style="margin-bottom: 20px">
+                                                    <a href="{{ url($value_category->slug) }}" class="menu-title">{{ $value_category->name }}</a>
+                                                    <ul>
+                                                        @foreach($value_category->getSubCategory as $value_subCategory)
+                                                        <li><a href="{{ url($value_category->slug.'/'.$value_subCategory->slug) }}">{{ $value_subCategory->name }}t</a></li>
+                                                        @endforeach
+                                                        </ul>
+                                                </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="product.html" class="sf-with-ul">Product</a>
+
+                            <div class="megamenu megamenu-sm">
+                                <div class="row no-gutters">
+                                    <div class="col-md-6">
+                                        <div class="menu-col">
+                                            <div class="menu-title">Product Details</div><!-- End .menu-title -->
+                                            <ul>
+                                                <li><a href="product.html">Default</a></li>
+                                                <li><a href="product-centered.html">Centered</a></li>
+                                                <li><a href="product-extended.html"><span>Extended Info<span class="tip tip-new">New</span></span></a></li>
+                                                <li><a href="product-gallery.html">Gallery</a></li>
+                                                <li><a href="product-sticky.html">Sticky Info</a></li>
+                                                <li><a href="product-sidebar.html">Boxed With Sidebar</a></li>
+                                                <li><a href="product-fullwidth.html">Full Width</a></li>
+                                                <li><a href="product-masonry.html">Masonry Sticky Info</a></li>
+                                            </ul>
+                                        </div><!-- End .menu-col -->
+                                    </div><!-- End .col-md-6 -->
+
+                                    <div class="col-md-6">
+                                        <div class="banner banner-overlay">
+                                            <a href="category.html">
+                                                <img src="assets/images/menu/banner-2.jpg" alt="Banner">
+
+                                                <div class="banner-content banner-content-bottom">
+                                                    <div class="banner-title text-white">New Trends<br><span><strong>spring 2019</strong></span></div><!-- End .banner-title -->
+                                                </div><!-- End .banner-content -->
+                                            </a>
+                                        </div><!-- End .banner -->
+                                    </div><!-- End .col-md-6 -->
+                                </div><!-- End .row -->
+                            </div><!-- End .megamenu megamenu-sm -->
+                        </li>
+                        <li>
+                            <a href="#" class="sf-with-ul">Pages</a>
+
+                            <ul>
+                                <li>
+                                    <a href="about.html" class="sf-with-ul">About</a>
+
+                                    <ul>
+                                        <li><a href="about.html">About 01</a></li>
+                                        <li><a href="about-2.html">About 02</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html" class="sf-with-ul">Contact</a>
+
+                                    <ul>
+                                        <li><a href="contact.html">Contact 01</a></li>
+                                        <li><a href="contact-2.html">Contact 02</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="login.html">Login</a></li>
+                                <li><a href="faq.html">FAQs</a></li>
+                                <li><a href="404.html">Error 404</a></li>
+                                <li><a href="coming-soon.html">Coming Soon</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="header-right">
+                <div class="header-search">
+                    <a href="#" class="search-toggle" role="button" title="Search"><i class="icon-search"></i></a>
+                    <form action="{{ url('search') }}" method="get">
+                        <div class="header-search-wrapper">
+                            <label for="q" class="sr-only">Search</label>
+                            <input type="search" class="form-control" name="q" id="q" placeholder="Search in..." required>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="dropdown cart-dropdown">
+                    <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
+                        <i class="icon-shopping-cart"></i>
+                        <span class="cart-count">2</span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <div class="dropdown-cart-products">
+                            <div class="product">
+                                <div class="product-cart-details">
+                                    <h4 class="product-title">
+                                        <a href="product.html">Beige knitted elastic runner shoes</a>
+                                    </h4>
+
+                                    <span class="cart-product-info">
+                                                <span class="cart-product-qty">1</span>
+                                                x $84.00
+                                            </span>
+                                </div>
+
+                                <figure class="product-image-container">
+                                    <a href="product.html" class="product-image">
+                                        <img src="{{ url('assets/images/products/cart/product-1.jpg') }}" alt="product">
+                                    </a>
+                                </figure>
+                                <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
+                            </div>
+
+                            <div class="product">
+                                <div class="product-cart-details">
+                                    <h4 class="product-title">
+                                        <a href="product.html">Blue utility pinafore denim dress</a>
+                                    </h4>
+
+                                    <span class="cart-product-info">
+                                                <span class="cart-product-qty">1</span>
+                                                x $76.00
+                                            </span>
+                                </div>
+
+                                <figure class="product-image-container">
+                                    <a href="product.html" class="product-image">
+                                        <img src="assets/images/products/cart/product-2.jpg" alt="product">
+                                    </a>
+                                </figure>
+                                <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
+                            </div>
+                        </div>
+
+                        <div class="dropdown-cart-total">
+                            <span>Total</span>
+
+                            <span class="cart-total-price">$160.00</span>
+                        </div>
+
+                        <div class="dropdown-cart-action">
+                            <a href="cart.html" class="btn btn-primary">View Cart</a>
+                            <a href="checkout.html" class="btn btn-outline-primary-2"><span>Checkout</span><i class="icon-long-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
