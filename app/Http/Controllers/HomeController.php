608ff8bd83d21ce7065e7b370d9217f978112042
MinMin <gow.bin.gg@gmail.com>
2024-06-29 13:05:51 +0700

    feat: create HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class HomeController extends Controller
 {
-
+    public function home()
+    {
+        return view('home');
+    }
 }
