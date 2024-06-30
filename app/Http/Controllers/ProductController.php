2430a0808acd4d5e64641fcb2398d978b1b5c2f5
MinMin <gow.bin.gg@gmail.com>
2024-06-30 21:38:14 +0700

    feat: add data and return to [Product List]

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,12 +16,22 @@ class ProductController extends Controller
         $getSubCategory = SubCategory::getSingleSlug($subslug);
 
         if (!empty($getCategory) && !empty($getSubCategory)) {
+            $data['meta_title'] = $getSubCategory->meta_title;
+            $data['meta_description'] = $getSubCategory->meta_description;
+            $data['meta_keywords'] = $getSubCategory->meta_keywords;
+
             $data['getCategory'] = $getCategory;
             $data['getSubCategory'] = $getSubCategory;
+
             return view('product.list', $data);
         }
         else if (!empty($getCategory)) {
             $data['getCategory'] = $getCategory;
+
+            $data['meta_title'] = $getCategory->meta_title;
+            $data['meta_description'] = $getCategory->meta_description;
+            $data['meta_keywords'] = $getCategory->meta_keywords;
+
             return view('product.list', $data);
         } else {
             abort(404);
