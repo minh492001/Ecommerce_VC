720f4682d96d13e57d863678168636644362ffa7
MinMin <gow.bin.gg@gmail.com>
2024-06-30 19:28:44 +0700

    feat: create [getCategory] feature for Shop view

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function getCategory($slug)
+    {
+        $getCategory = Category::getSingleSlug($slug);
 
+        if (!empty($getCategory)) {
+            $data['getCategory'] = $getCategory;
+            return view('product.list', $data);
+        } else {
+            abort(404);
+        }
+    }
 }
