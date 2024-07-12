57914fee722997602fcc906fff15e9d6cbbd08e8
MinMin <gow.bin.gg@gmail.com>
2024-07-13 00:50:06 +0700

    feat: create [Place Order] feature

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,12 +6,16 @@ use App\Models\Color;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\ShippingCharge;
+use App\Models\User;
 use Cart;
 use Illuminate\Http\Request;
 
 use App\Models\Product;
 use App\Models\ProductSize;
 use App\Models\DiscountCode;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 class PaymentController extends Controller
 {
     public function add_to_cart(Request $request)
@@ -107,5 +111,114 @@ class PaymentController extends Controller
     {
         Cart::remove($id);
         return redirect()->back();
+    }
+
+    public function place_order(Request $request)
+    {
+        $validate = 0;
+        $message = '';
+
+        // Check if Login or not
+        if (!empty(Auth::check())) {
+            $user_id = Auth::user()->id;
+        } else {
+            // Check Email Exist or Not
+            if (!empty($request->is_create)) {
+                $checkEmail = User::checkEmail($request->email);
+                if (!empty($checkEmail)) {
+                    $message = "Email Already Exist !";
+                    $validate = 1;
+                } else {
+                    // Create User
+                    $user = new User;
+                    $user->name = trim($request->first_name);
+                    $user->email = trim($request->email);
+                    $user->password = Hash::make($request->password);
+                    $user->save();
+                    $user_id = $user->id;
+                }
+            } else {
+                $user_id = '';
+            }
+        }
+
+        // If create account success then create Order for new account
+        if (empty($validate)) {
+            $getShipping = ShippingCharge::getSingle($request->shipping);
+            $payable_total = Cart::getSubTotal();
+            $discount_amount = 0;
+            $discount_code = '';
+
+            // Calculate Total Price
+            if (!empty($request->discount_code)) {
+                $getDiscount = DiscountCode::checkDiscount($request->discount_code);
+                if (!empty($getDiscount)) {
+                    $discount_code = $request->discount_code;
+                    if ($getDiscount->type == 'Amount') {
+                        $discount_amount = $getDiscount->percent_amount;
+                        $payable_total = $payable_total - $discount_amount;
+                    } else {
+                        $discount_amount = ($payable_total * $getDiscount->percent_amount) / 100;
+                        $payable_total = $payable_total - $discount_amount;
+                    }
+                }
+            }
+            $shipping_amount = !empty($getShipping->price) ? $getShipping->price : 0;
+            $total_amount = $payable_total + $shipping_amount;
+
+            // Create Order
+            $order = new Order;
+            if (!empty($user_id)) {
+                $order->user_id = trim($user_id);
+            }
+            $order->first_name = trim($request->first_name);
+            $order->last_name = trim($request->last_name);
+            $order->company_name = trim($request->company_name);
+            $order->country = trim($request->country);
+            $order->address_one = trim($request->address_one);
+            $order->address_two = trim($request->address_two);
+            $order->city = trim($request->city);
+            $order->state = trim($request->state);
+            $order->postcode = trim($request->postcode);
+            $order->phone = trim($request->phone);
+            $order->email = trim($request->email);
+            $order->note = trim($request->note);
+            $order->discount_code = trim($discount_code);
+            $order->discount_amount = trim($discount_amount);
+            $order->shipping_id = trim($request->shipping);
+            $order->shipping_amount = trim($shipping_amount);
+            $order->total_amount = trim($total_amount);
+            $order->payment_method = trim($request->payment_method);
+            $order->save();
+
+            foreach(Cart::getContent() as $key => $cart){
+                $order_item = new OrderItem;
+                $order_item->order_id = $order->id;
+                $order_item->product_id = $cart->id;
+                $order_item->quantity = $cart->quantity;
+                $order_item->price = $cart->price;
+
+                $color_id = $cart->attributes->color_id;
+                if (!empty($color_id)) {
+                    $getColor = Color::getSingle($color_id);
+                    $order_item->color_name = $getColor->name;
+                }
+
+                $size_id = $cart->attributes->size_id;
+                if (!empty($size_id)) {
+                    $getSize = ProductSize::getSingle($size_id);
+                    $order_item->size_name = $getSize->name;
+                    $order_item->size_amount = $getSize->price;
+                }
+                $order_item->total_price = $cart->price;
+                $order_item->save();
+            }
+            $json['status'] = true;
+            $json['message'] = 'Order placed successfully !';
+        } else {
+            $json['status'] = false;
+            $json['message'] = $message;
+        }
+        echo json_encode($json);
     }
 }
