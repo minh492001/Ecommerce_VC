6887330f4336b9e3281e9e228aa48f03480c08f0
MinMin <gow.bin.gg@gmail.com>
2024-07-15 17:52:32 +0700

    feat: create [Payment] feature

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,4 +10,9 @@ class Order extends Model
     use HasFactory;
 
     protected $table = 'orders';
+
+    static public function getSingle($order_id)
+    {
+        return self::find($order_id);
+    }
 }
