f2ca590928150a956009886add251547618bac6c
MinMin <gow.bin.gg@gmail.com>
2024-07-24 16:13:11 +0700

    feat: create [Order Detail] view for Customer

--- routes/web.php
+++ routes/web.php
@@ -137,6 +137,7 @@ Route::group(['middleware' => 'admin'], function () {
 Route::group(['middleware' => 'user'], function () {
     Route::get('user/dashboard', [UserController::class, 'dashboard']);
     Route::get('user/orders', [UserController::class, 'orders']);
+    Route::get('user/orders/detail/{id}', [UserController::class, 'orders_detail']);
     Route::get('user/edit-profile', [UserController::class, 'edit_profile']);
     Route::get('user/change-password', [UserController::class, 'change_password']);
 });
